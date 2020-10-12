5a055d238e78fbe63724bd017115848264d3a119
Alan Lüdke <alanludke@gmail.com>
2020-10-12 17:20:16 -0300

    6x6 working!! next episode: 9x9

--- str8ts.hs
+++ str8ts.hs
@@ -1,90 +1,85 @@
--- Wynand van Dyk - September 2012
--- Recursive backtracking straights solver
--- 
--- Load this file up in ghci with the following command:
--- :l straights.hs
---
--- Run the solver on the puzzle and print out the solution:
--- putStrLn $ pPrint $ solveIt straights
+-- ghci str8ts
+-- table (chunksOf 6 (solveIt str8tsBoard gapsBoard))
 
-module Straights where
-import Data.List
+module Str8ts where
+import Data.List ( nub, transpose )
 import Data.List.Split
+    ( chunksOf, condense, dropBlanks, split, whenElt )
 
---The puzzle we are trying to solve
-straights2 :: [Int]
-straights2 = [4, 9, 0, 0, 1, 9,
-             0, 0, 0, 0, 0, 9,
-             0, 0, 9, 0, 0, 5,
-             2, 0, 0, 9, 0, 0,
-             9, 3, 0, 0, 0, 6,
-             9, 0, 0, 0, 9, 1]
+--https://www.janko.at/Raetsel/Straights/105.a.htm
+str8tsBoard_6x6 :: [Int]
+str8tsBoard_6x6 = [-1, 0, 0, 0, -1, 4,
+                   -1, 0, 0, 0, 0, 1,
+                    0, 0, 3, -1, 0, 0,
+                    5, 0, -1, -1, 0, 0,
+                    6, 0, 4, 0, 0, -1,
+                   -1, 1, 0, 0, 0, -1]
 
-answer :: [Int]
-answer = [4, 9, 2, 3, 1, 9,
-          3, 5, 1, 2, 4, 9,
-          1, 2, 9, 4, 3, 5,
-          2, 1, 3, 9, 5, 4,
-          9, 3, 4, 5, 2, 6,
-          9, 4, 5, 6, 9, 1]
+gapsBoard_6x6 :: [Int]
+gapsBoard_6x6 = [0, 1, 1, 1, 0, 0,
+                 0, 1, 1, 1, 1, 1,
+                 1, 1, 0, 0, 1, 1,
+                 1, 1, 0, 0, 1, 1,
+                 1, 1, 1, 1, 1, 0,
+                 0, 0, 1, 1, 1, 0]
+----------------------------------------------------------------
+--https://www.janko.at/Raetsel/Straights/100.a.htm
+str8tsBoard_9x9 :: [Int]
+str8tsBoard_9x9 = [0, 0, -1, -1, 0, 0, -1, 0, 0,
+                   6, 0, 0, 0, 0, -1, 0, 3, 0,
+                   -1, 0, 0, 0, 0, 4, 0, 0, 0,
+                   0, 0, 4, -1, 0, 0, 0, 0, -1,
+                   0, 0, 0, 0, 0, 0, 0, 0, 0,
+                   1, 0, 0, 0, 0, -1, 7, 9, 0,
+                   0, 0, 0, 8, 0, 0, 0, 0, -1,
+                   0, 0, 0, -1, 9, 0, 0, 5, 0,
+                   0, 0, 9, 0, 0, 5, -1, 0, 0]
 
-gapsBoard2 :: [Int]
-gapsBoard2 = [0, 0, 1, 1, 1, 0,
-             1, 1, 1, 1, 1, 0,
-             1, 1, 0, 1, 1, 1,
-             1, 1, 1, 0, 1, 1,
-             0, 1, 1, 1, 1, 1,
-             0, 1, 1, 1, 0, 0]
+gapsBoard_9x9 :: [Int]
+gapsBoard_9x9 = [1, 1, 0, 0, 1, 1, 0, 1, 1,
+                 1, 1, 1, 1, 1, 0, 1, 1, 1,
+                 0, 1, 1, 1, 1, 0, 1, 1, 1,
+                 1, 1, 0, 0, 1, 1, 1, 1, 0,
+                 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                 0, 1, 1, 1, 1, 0, 0, 1, 1,
+                 1, 1, 1, 0, 1, 1, 1, 1, 0,
+                 1, 1, 1, 0, 1, 1, 1, 1, 1,
+                 1, 1, 0, 1, 1, 0, 0, 1, 1]
 
+boardSize :: [Int] -> Int
+boardSize [] = 0
+boardSize s = length(s)
 
-gapsBoard3 :: [Int]
-gapsBoard3 = [0, 1, 1, 1, 0, 0,
-              0, 1, 1, 1, 1, 1,
-              1, 1, 0, 0, 1, 1,
-              1, 1, 0, 0, 1, 1,
-              1, 1, 1, 1, 1, 0,
-              0, 0, 1, 1, 1, 0]
+lineWidth :: [Int] -> Int
+lineWidth [] = 0
+lineWidth s = intSquareRoot (boardSize s)
 
-straights :: [Int]
-straights = [9, 0, 0, 1, 9, 9,
-            9, 0, 0, 0, 5, 0,
-            9, 0, 1, 0, 0, 0,
-            4, 0, 0, 0, 0, 9,
-            0, 6, 5, 0, 0, 9,
-            9, 9, 9, 0, 1, 4]
-
-gapsBoard :: [Int]
-gapsBoard = [0, 1, 1, 0, 0, 0,
-             0, 1, 1, 1, 1, 1,
-             0, 1, 1, 1, 1, 1,
-             1, 1, 1, 1, 1, 0,
-             1, 1, 1, 1, 1, 0,
-             0, 0, 0, 1, 1, 0]
+intSquareRoot :: Int -> Int
+intSquareRoot n = try n where
+  try i   | i*i > n   = try (i - 1) 
+          | i*i <= n  = i
 
 -- converts an index i into an x and y co-ordinate
-itop :: Int -> (Int, Int)
-itop i = (calcX i, calcY i)
-  where calcX i   = i - 6 * (i `div` 6)
-        calcY i   = i `div` 6
+indexToCoord :: Int -> Int -> (Int, Int)
+indexToCoord i l= (calcX i l, calcY i l)
+  where calcX i l  = i - l * (i `div` l)
+        calcY i l  = i `div` l
 
 -- Takes an x and y co-ordinate and converts it into an index
-ptoi :: (Int, Int) -> Int
-ptoi (x, y) = x + y * 6
+coordToIndex :: (Int, Int) -> Int -> Int
+coordToIndex (x, y) l= x + y * l
 
 -- Retrieves the vertical column of values from the board (s) at the index (p)
 columnAt :: Int -> [Int] -> [Int]
-columnAt p s = helperColumnAt (itop p) s
-  where helperColumnAt (x, _) s = map (\y -> s !! ptoi (x, y)) [0..5]
+columnAt p s = helperColumnAt (indexToCoord p (lineWidth s)) s
+  where helperColumnAt (x, _) s = map (\y -> s !! coordToIndex (x, y) (lineWidth s)) [0..((lineWidth s)-1)] 
 
 -- Retrieves the horizontal row of values from the board (s) at the index (p)
 rowAt :: Int -> [Int] -> [Int]
-rowAt p s = helperRowAt (itop p) s
-  where helperRowAt (_, y) s = map (\x -> s !! ptoi (x, y)) [0..5]
-
+rowAt p s = helperRowAt (indexToCoord p (lineWidth s)) s
+  where helperRowAt (_, y) s = map (\x -> s !! coordToIndex (x, y) (lineWidth s)) [0..((lineWidth s)-1)]
 
 -- Removes the elements in the second list from the first list
--- first list - second list
--- remove [9,0,0,0,9,1] [9,9] = [0,0,0,1]
 remove :: [Int] -> [Int] -> [Int]
 remove [] _       = []
 remove xs []      = xs
@@ -99,10 +94,9 @@ removeAll y (x:xs) | x == y    = removeAll y xs
 -- The list of solutions at the index p of board s
 solutionsAt :: Int -> [Int] -> [Int]
 solutionsAt p s | p > length s  = []
-                | ((s !! p) == 0) = [1..6] `remove` (columnAt p s ++ rowAt p s)
+                | ((s !! p) == 0) = [1..(lineWidth s)] `remove` (columnAt p s ++ rowAt p s)
                 | otherwise     = [s !! p]
 
---[n | n <- [1..6], not $ any (n==) (getRow index board), not $ any (n==) (getColumn index board)]
 noGaps :: (Enum a, Ord a) => [a] -> Bool
 noGaps [] = True
 noGaps xs = all (`elem` xs) [minimum xs .. maximum xs]
@@ -119,14 +113,15 @@ parseList (a:b) (c:d) = if notBlack c then
                         else parseList b d
 
 notBlack :: [Int] -> Bool 
-notBlack (a:b) = if a == 1 then True else False
+notBlack (a:_) = if a == 1 then True else False
 
 -- Generate a new version of board s with value x inserted at index p
-tryWith :: Int -> [Int] -> Int -> [Int]
-tryWith 35 s x | ((parseList (splitManySizes (sizes (splitIn0and1 gapsBoard)) s ) (splitIn0and1 gapsBoard)) == True) && 
-                 ((parseList (splitManySizes (sizes (splitIn0and1Transposed gapsBoard)) (joinTranspose (splitIn0and1Transposed s)) ) (splitIn0and1Transposed gapsBoard)) == True)  = take 35 s ++ [x] ++ drop (35 + 1) s
-               | otherwise        = []
-tryWith p s x = take p s ++ [x] ++ drop (p + 1) s
+tryWith :: Int -> [Int] -> [Int] -> Int -> [Int]
+tryWith p s g x | (p == ((boardSize s) - 1)) && normalParse && transposedParse = take ((boardSize s) - 1) s ++ [x] ++ drop (boardSize s) s
+                | (p /= ((boardSize s) - 1))                                   = take p s ++ [x] ++ drop (p + 1) s
+                | otherwise                                   = []
+  where normalParse =  ((parseList (splitManySizes (sizes (splitIn0and1 g)) s ) (splitIn0and1 g)) == True)
+        transposedParse = ((parseList (splitManySizes (sizes (splitIn0and1Transposed g)) (joinTranspose (splitIn0and1Transposed s))) (splitIn0and1Transposed g)) == True)
 
 --Junta o gapsBoard em uma lista unica, para poder ser usado no splitManySizes
 joinTranspose :: [[a]] -> [a]
@@ -136,11 +131,11 @@ joinTranspose (a:b) = a ++ joinTranspose b
 
 --Separa o gapsBoard em listas de 0 e 1
 splitIn0and1 :: [Int] -> [[Int]]
-splitIn0and1 a = separateZeros(chunksOf 6(a))
+splitIn0and1 a = separateZeros(chunksOf (lineWidth a) a)
 
 --Separa a transposta do gapsBoard em listas de 0 e 1
 splitIn0and1Transposed :: [Int] -> [[Int]]
-splitIn0and1Transposed a = separateZeros(transpose (chunksOf 6(a)))
+splitIn0and1Transposed a = separateZeros(transpose (chunksOf (lineWidth a) a))
 
 --Separa em listas de 0 e 1
 separateZeros :: [[Int]] -> [[Int]]
@@ -162,38 +157,34 @@ splitManySizes [] _ = [[]]
 splitManySizes (a:b) d = [take a d] ++ splitManySizes b (drop a d)
 
 -- Find the next blank value starting from index p on board s
--- 35 is the index of the last element in s
-nextBlank :: Int -> [Int] -> Int
-nextBlank p s | p == 35           = 35
-              | (s !! (p + 1) == 0) && (gapsBoard !! (p+1) == 1) = p + 1
-              | otherwise         = nextBlank (p + 1) s
+nextBlank :: Int -> [Int] -> [Int] -> Int
+nextBlank p s g | p == ((boardSize s)-1)           = ((boardSize s)-1)
+                | (s !! (p + 1) == 0) && (g !! (p+1) == 1) = p + 1
+                | otherwise         = nextBlank (p + 1) s g
               
 -- Recursively try and brute-force solve the board given in s, starting at p,
 -- with the set of possible solutions at that point.
--- 35 is the index of the last element in s
-solve :: Int -> [Int] -> [Int] -> [Int]
-solve 35 s []     = []
-solve 35 s (x:[]) = tryWith 35 s x
-solve 35 s (x:_)  = []
-solve _  s []     = []
-solve p s (x:xs)  | solvedNext == [] = solve p s xs
-                  | otherwise        = solvedNext                 
-  where solveNext p s = solve (nextBlank p s) s (solutionsAt (nextBlank p s) s)
-        solvedNext    = solveNext p (tryWith p s x)
+solve :: Int -> [Int] -> [Int] -> [Int] -> [Int]
+solve p s g h  | h == [] = []
+               | (p == ((boardSize s) - 1)) && (tail h /= []) = []
+               | (p == ((boardSize s) - 1)) && (tail h == []) = tryWith ((boardSize s)-1) s g (head h)
+               | solvedNext == [] = solve p s g (tail h)
+               | otherwise        = solvedNext                 
+  where solveNext p s g = solve (nextBlank p s g) s g (solutionsAt (nextBlank p s g) s)
+        solvedNext    = solveNext p (tryWith p s g (head h)) g
 
-solveIt s = solve 0 s (solutionsAt 0 s)
+solveIt :: [Int] -> [Int] -> [[Int]]
+solveIt s g = chunksOf (lineWidth s) (solve 0 s g (solutionsAt 0 s))
 
 -- intersperse the element c through-out the string xs
 joinWith :: a -> [a] -> [a]
 joinWith _ (x:[])  = [x]
 joinWith c (x:xs)  = x : c : joinWith c xs
 
--- Pretty-print the board as a spaced out 6 x 6 square
-pPrint [] = []
-pPrint s  = spaceOut s ++ pPrint (drop 6 s)
-  where showS s    = concatMap show s
-        space      = ' '
-        newline    = "\n"
-        spaceOut s = joinWith space (take 6 (showS s) ++ newline)
-   
-   
+print_ :: Show a => a -> IO ()
+print_ x =  putStr $ (show x) ++ "\t" 
+
+table :: (Foldable t, Show a) => [t a] -> IO ()
+table xxs | length (nub [length xs | xs <- xxs])/=1 = error "not simetric"
+          | otherwise = mapM_ printRow xxs 
+            where printRow xs =  (mapM_ print_) xs >> putStrLn "" 
